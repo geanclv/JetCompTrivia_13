90c2a381fa8aeae9aeba94554e860e09995c21b4
geanclv <geanclv@gmail.com>
2022-09-03 16:09:55 -0500

    Updating AppModule with QuestionApi provider fun.

--- app/src/main/java/com/geancarloleiva/jetcomptrivia_13/di/AppModule.kt
+++ app/src/main/java/com/geancarloleiva/jetcomptrivia_13/di/AppModule.kt
@@ -1,12 +1,27 @@
 package com.geancarloleiva.jetcomptrivia_13.di
 
+import com.geancarloleiva.jetcomptrivia_13.network.QuestionApi
+import com.geancarloleiva.jetcomptrivia_13.util.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    //here we will gonna create the providers
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionApi{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionApi::class.java)
+    }
+
 }
