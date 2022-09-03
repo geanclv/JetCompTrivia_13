df22c3c9c89c12b3a931a275e5c2d2527b8d02a7
geanclv <geanclv@gmail.com>
2022-09-03 16:00:06 -0500

    Creating Constant object.

---
+++ app/src/main/java/com/geancarloleiva/jetcomptrivia_13/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.geancarloleiva.jetcomptrivia_13.util
+
+object Constants {
+
+    //main URL: https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/world.json
+    const val BASE_URL = "https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/"
+    const val QUESTION_URL = "world.json"
+}
