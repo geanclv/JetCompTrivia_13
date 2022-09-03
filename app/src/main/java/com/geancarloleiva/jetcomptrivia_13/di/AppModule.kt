a52ae8561235f263b549c522abe78355ddb1d23e
geanclv <geanclv@gmail.com>
2022-09-03 15:56:58 -0500

    Creating AppModule, where we gonna create the providers later.

---
+++ app/src/main/java/com/geancarloleiva/jetcomptrivia_13/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.geancarloleiva.jetcomptrivia_13.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    //here we will gonna create the providers
+}
